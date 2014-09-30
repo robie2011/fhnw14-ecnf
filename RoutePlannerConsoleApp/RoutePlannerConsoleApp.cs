12dbf0c89e2388b1ee9d09998a726c4c2653da1e
Robert Rajakone <robie2011@gmail.com>
2014-09-30 21:15:34 +0200

    after some changes

---
+++ RoutePlannerConsoleApp/RoutePlannerConsoleApp.cs
@@ -0,0 +1,28 @@
+﻿using RoutePlannerLiFhnw.Ecnf.RoutePlanner.RoutePlannerLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
+{
+    class RoutePlannerConsoleApp
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(assembly.Location);
+            string version = fvi.FileVersion;
+            Console.WriteLine("Welcome to RoutePlanner ({0})", version);
+
+            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
+
+            Console.ReadKey();
+        }
+    }
+}
