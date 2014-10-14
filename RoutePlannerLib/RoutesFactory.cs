00d7ef3fa61f71a1a1c22062d10955b4c0663698
JoergSteiner <l.kessler@outlook.com>
2014-10-14 14:38:59 +0200

    added tests, settings property and RouteFactory

---
+++ RoutePlannerLib/RoutesFactory.cs
@@ -0,0 +1,24 @@
+﻿using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoutePlannerLiFhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    public class RoutesFactory
+    {
+        static public IRoutes Create(Cities cities)
+        {
+            //TODO
+            return null;
+        }
+
+        static public IRoutes Create(Cities cities, string algorithmClassName)
+        {
+            //TODO
+            return null;
+        } 
+    }
+}
