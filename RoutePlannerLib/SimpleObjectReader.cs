1b7dfbf00a800df6b70f699a34c3c5f666805a27
Robert Rajakone <robie2011@gmail.com>
2014-10-26 09:48:25 +0100

    create

---
+++ RoutePlannerLib/SimpleObjectReader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    public class SimpleObjectReader
+    {
+        public SimpleObjectReader(StringReader sb)
+        {
+
+        }
+
+        public City Next()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
